9c6e77aabd792e8a6665d6cf86822a3c4a1fe28b
Rob Prouse <rob@prouse.org>
2024-04-11 07:56:06 -0400

    Refactor print.asm to use consistent commenting style and add header guard

--- print.asm
+++ print.asm
@@ -1,6 +1,7 @@
-;
+%ifndef _print_h_
+%define _print_h_
+
 ; Prints a string pointed to by the BX register using the BIOS
-;
 print_string:
     pusha
     mov si, bx      ; si = bx
@@ -15,17 +16,15 @@ print_string:
     popa
     ret
 
-
-;
 ; Prints the value of DX as a hexadecimal number
 print_hex:
     pusha
-    mov ah, 0x0E    ; int =10/ah=0x0e -> BIOS teletype function
+    mov ah, 0x0E      ; int =10/ah=0x0e -> BIOS teletype function
     mov al, '0'
     int 0x10
     mov al, 'x'
     int 0x10
-    mov ecx, 4
+    mov ecx, 4        ; 4 nibbles in a word
 .next_digit:
     mov ax, dx
     and ax, 0x000F
@@ -36,11 +35,13 @@ print_hex:
 .is_digit:
     add al, '0'
 .print:
-    mov ah, 0x0E    ; int =10/ah=0x0e -> BIOS teletype function
+    mov ah, 0x0E      ; int =10/ah=0x0e -> BIOS teletype function
     int 0x10
     shr dx, 4
-    loop .next_digit
+    loop .next_digit  ; loop until all nibbles are printed
     popa
     ret
 
 HEX_OUT: db "0x0000", 0
+
+%endif
