59a451c5cdcd0cf966882e4b0cec9c611f3ccedb
Rob Prouse <rob.prouse@aspiresoftware.com>
2024-05-23 20:54:33 -0400

    Simple compilation

---
+++ tests/basic.c
@@ -0,0 +1,6 @@
+// gcc -ffreestanding -m32 -c basic.c -o basic.o
+// ld -T NUL -o basic.tmp -Ttext 0x1000 -m i386pe basic.o
+// objcopy -O binary -j .text  basic.tmp basic.bin
+int my_function() {
+    return 0xDEAD;
+}
