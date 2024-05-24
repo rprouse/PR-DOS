aadafb47aecc8b73855348fda73917b98f99795b
Rob Prouse <rob.prouse@aspiresoftware.com>
2024-05-23 21:08:42 -0400

    Function calling convention

--- tests/basic.c
+++ tests/basic.c
@@ -1,7 +1,10 @@
 // gcc -ffreestanding -m32 -c basic.c -o basic.o
 // ld -T NUL -o basic.tmp -Ttext 0x1000 -m i386pe basic.o
 // objcopy -O binary -j .text  basic.tmp basic.bin
-int my_function() {
-    int my_var = 0xDEAD;
-    return my_var;
+int callee_function(int my_arg) {
+    return my_arg;
+}
+
+void caller_function() {
+    callee_function(0xDEAD);
 }
